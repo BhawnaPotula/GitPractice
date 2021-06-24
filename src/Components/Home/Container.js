052986315d7a0557f7a194165f9b152a835d0215
BhawnaPotula <216bhawna@gmail.com>
2021-06-24 23:09:46 +0530

    Navigstion added

--- src/Components/Home/Container.js
+++ src/Components/Home/Container.js
@@ -3,6 +3,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 
+//Navigation testing
 export default function SimpleContainer() {
   return (
     <React.Fragment>
@@ -10,7 +11,7 @@ export default function SimpleContainer() {
       <Container maxWidth="sm">
         <Typography
           component="div"
-          style={{ backgroundColor: "#000000", height: "100vh" }}
+          style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
         />
       </Container>
     </React.Fragment>
